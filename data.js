638b270417770feb8a81eb9d326ce9561bb5c1ef
iambittuzz <89698917+iambittuzz@users.noreply.github.com>
2022-03-12 19:36:59 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = {
+  token:'5282006556:AAF_F-ZLWxQzhb0bxtlD4xFTvtx0tYiCRBM', // token from @BotFather
+  mongoLink: 'mongodb+srv://Visnu0p:1751@bittu.gv1tl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
+  admin: '1211389957', // admin`s id for sending him/her errors,
+  cp_key:'ecec5f339bc8e157053ea1fd19bad74f7a5fdb2ff1769584242719094b8efbcb',
+  cp_secret:'5f4E6fa6C8E66d582de2aAf68DF81202e3f47Df1F8ce26c60d147b18981E524B',
+  currency:'DGB',
+  min_wd:0.05,// minimum withdrawal amount
+  refer_bonus0.0.5,
+  max_wd:0.05,
+  daily_bonus:0.05 ,
+  payment_channel:'@universepayout',
+  bot_username:'DgbMegaVbot',
+  channels: ['@Bsclooters']
+}
+
